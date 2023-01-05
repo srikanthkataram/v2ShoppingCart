7493212cb4fa71aec1a497d15e135caaf35a817c
Srikanth Kataram <SrikanthKataram@ncfe.org.uk>
2023-01-05 22:08:29 +0000

    Add project files.

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace v2ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        // Create a new shopping cart with initial values
+        private ShoppingCart cart = new ShoppingCart(new List<string> { "Apple", "Banana" }, new List<double> { 0.99, 0.59 }, new List<int> { 2, 3 });
+
+        // Add an item to the cart
+        [HttpPost("add")]
+        public void AddItem(string item, double price, int quantity)
+        {
+            cart.AddItem(item, price, quantity);
+        }
+
+        // Get the total cost of the items in the cart
+        [HttpGet("total")]
+        public double GetTotalCost()
+        {
+            return cart.GetTotalCost();
+        }
+
+        // Get a list of all items in the cart
+        [HttpGet("items")]
+        public List<string> GetItems()
+        {
+            return cart.GetItems();
+        }
+
+        [HttpGet]
+        public ShoppingCart GetCart()
+        {
+            return cart;
+        }
+
+
+        // Remove an item from the cart
+        [HttpDelete("remove")]
+        public void RemoveItem(string item)
+        {
+            cart.RemoveItem(item);
+        }
+    }
+}
---
+++ ShoppingCart.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace v2ShoppingCart
+{   
+    public class ShoppingCart
+    {
+        // A list to hold the items in the cart
+        private List<string> items;
+
+        // A list to hold the prices of the items in the cart
+        private List<double> prices;
+
+        // A list to hold the quantities of the items in the cart
+        private List<int> quantities;
+
+        public ShoppingCart()
+        {
+            // Initialize the lists
+            items = new List<string>();
+            prices = new List<double>();
+            quantities = new List<int>();
+        }
+
+        public ShoppingCart(List<string> items, List<double> prices, List<int> quantities)
+        {
+            // Set the fields to the given values
+            this.items = items;
+            this.prices = prices;
+            this.quantities = quantities;
+        }
+        
+        public void AddItem(string item, double price, int quantity)
+        {
+            // Add the item, price, and quantity to the lists
+            items.Add(item);
+            prices.Add(price);
+            quantities.Add(quantity);
+        }
+
+        public double GetTotalCost()
+        {
+            // Initialize a variable to hold the total cost
+            double totalCost = 0;
+
+            // Loop through the items in the cart
+            for (int i = 0; i < items.Count; i++)
+            {
+                // Add the cost of the item to the total cost
+                totalCost += prices[i] * quantities[i];
+            }
+
+            // Return the total cost
+            return totalCost;
+        }
+
+        public List<string> GetItems()
+        {
+            // Return the list of items
+            return items;
+        }
+
+        public void RemoveItem(string item)
+        {
+            // Find the index of the item in the list
+            int index = items.IndexOf(item);
+
+            // If the item was found, remove it from the lists
+            if (index != -1)
+            {
+                items.RemoveAt(index);
+                prices.RemoveAt(index);
+                quantities.RemoveAt(index);
+            }
+        }
+    }
+}
